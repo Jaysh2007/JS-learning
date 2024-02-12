fc378404da05d5992b3b95b342161c90af84e83b
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-02-12 17:33:42 +0530

    String_char

---
+++ String_charcterstics.js
@@ -0,0 +1,11 @@
+// String characterstics & roperties
+
+let str = "Javascript is wonderful";
+let str2 = "It is dynamically typed";
+
+console.log(str._proto_);
+
+console.log(str.charAt(7)); //Tells the charcter present at index 7.
+console.log(str.concat(str2)); // Join str and str2.
+console.log(str.indexOf("is")); // Tell's the index number from which "is" is starting.
+console.log(str.length); // Tell's the length of the given string 'str'.
