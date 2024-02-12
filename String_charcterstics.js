c912cce34c3f55af067e0e2b61356d5a6de9ee5a
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-02-12 17:35:46 +0530

    edit

--- String_charcterstics.js
+++ String_charcterstics.js
@@ -9,3 +9,4 @@ console.log(str.charAt(7)); //Tells the charcter present at index 7.
 console.log(str.concat(str2)); // Join str and str2.
 console.log(str.indexOf("is")); // Tell's the index number from which "is" is starting.
 console.log(str.length); // Tell's the length of the given string 'str'.
+console.log(str.indexOf("i",8)); // It will give the index number of "i" present after index number 8.
