155544a7c8eac78a94a9c2fe39eeaed03a952d1b
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-02-13 16:55:49 +0530

    Logical operators

---
+++ Logical operators.js
@@ -0,0 +1,20 @@
+// Logical operators
+
+// let grade = 65;
+// let scholarship = 80;
+// let eligible = grade>60 && scholarship >70;
+// console.log(eligible); output will be false
+
+
+// let m1 = 55;
+// let m2 = 65;
+// let m3 = 45;
+// let eligibility = m1>60 || m2 > 60 || m3 > 60;
+// console.log(eligibility); output will be true bcoz m2 > 60;
+
+
+
+// Truthy values and falsy values
+
+// Truthy values = "hello! world",non-zero-numbers,non-empty-arrays,non-empyt-objects;
+// falsy values = (false, 0, "", null, undefined, and NaN) 
