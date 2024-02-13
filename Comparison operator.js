e6f32a0ae3d960619f3cf6b384d8a57031668b61
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-02-13 22:15:30 +0530

    new

--- Comparison operator.js
+++ Comparison operator.js
@@ -5,7 +5,7 @@
 // console.log(5==6) is equal to false;
 // console.log(5!=5) is equal to false;
 // console.log (5<=5) is equal to true;
-// console.log('5'=5) is equal to true bcoz js is converting datatypes to match the CSSFontFeatureValuesRule;
+// console.log('5'=5) is equal to true bcoz js is converting datatypes to match the values;
 // console.log('5'===5) it will give false;
 // console.log('6'!==6) it will give false;
 let birthYear = 17;
