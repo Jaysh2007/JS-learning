9740fab5d4519f4719c73d75b903f1601bf4658e
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-02-13 16:30:19 +0530

    comparison operator

---
+++ Comparison operator.js
@@ -0,0 +1,13 @@
+// operators in javascript
+
+// (==,!=,<,<=,>,>=,===,!==)
+
+// console.log(5==6) is equal to false;
+// console.log(5!=5) is equal to false;
+// console.log (5<=5) is equal to true;
+// console.log('5'=5) is equal to true bcoz js is converting datatypes to match the CSSFontFeatureValuesRule;
+// console.log('5'===5) it will give false;
+// console.log('6'!==6) it will give false;
+let birthYear = 17;
+let rightToVote = birthYear >=18;
+console.log(rightToVote);
